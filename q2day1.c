856143b2e38a03d47ff476fe192213349431040c
Aaditya singh <aadityasingh69344@gmail.com>
2026-08-15 23:47:52 +0530

    Add files via upload

---
+++ q2day1.c
@@ -0,0 +1,21 @@
+/*
+Sample Test Cases:
+Input 1:
+10 2
+Output 1:
+Sum=12, Diff=8, Product=20, Quotient=5
+
+Input 2:
+7 3
+Output 2:
+Sum=10, Diff=4, Product=21, Quotient=2
+
+*/
+#include <stdio.h>
+int main () {
+int num1, num2;
+printf("enter two numbers\n");
+scanf("%d %d", &num1, &num2);
+printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d", num1 + num2, num1 - num2, num1 * num2, num1 / num2);
+return 0;
+}
